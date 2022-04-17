b4edad8903b167ee3319e77250ba033fcd6b4866
Le Tra <71242240+lephanthutra@users.noreply.github.com>
2022-04-17 18:07:57 +0700

    Create Binary Tree Nodes.sql

---
+++ Binary Tree Nodes.sql
@@ -0,0 +1,7 @@
+select case 
+            when P is NULL then concat(N, ' Root')
+            when N in (select distinct P from BST) then concat(N, ' Inner')
+            else concat(N, ' Leaf')
+       end
+from BST
+order by N asc
