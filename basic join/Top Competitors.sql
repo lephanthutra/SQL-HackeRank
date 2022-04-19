7b0dbdc685367abb53d565d5bdede7eda213dd1e
Le Tra <71242240+lephanthutra@users.noreply.github.com>
2022-04-19 10:49:11 +0700

    Create Top Competitors.sql

---
+++ basic join/Top Competitors.sql
@@ -0,0 +1,9 @@
+select s.hacker_id, h.name
+from submissions as s
+join hackers as h on s.hacker_id = h.hacker_id
+join challenges as c on s.challenge_id = c.challenge_id
+join difficulty as d on c.difficulty_level = d.difficulty_level
+where s.score = d.score
+group by s.hacker_id, h.name
+having count(*) > 1
+order by count(*) desc, s.hacker_id asc
